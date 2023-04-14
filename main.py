6fb0d2bb7b5413f453bef94ae436edf7b1e503ca
realysi <116087413+realysi@users.noreply.github.com>
2023-04-14 23:54:32 +0200

    Functies toegevoegd
    -Functies om datum en tijd om te zetten in seconden en vice versa
    - Functie om te selecteren op soort
    - Functie om te selecteren op ID
    
    Idee is om seconde vanaf 1970 te pakken en de intervals hierop los te laten. Als de seconden matchen van twee sightings, dan telt dit als 1 spotting en eindigt de spotting binnen de interval van de eerste spotting.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from code.reading_data_voorbeelden import read_data
-from code.intervals import time_to_seconds
+from code.intervals import hours_to_seconds, seconds_to_hours, dates_to_seconds, select_row_species
+from code.intervals import select_ID
+import datetime
 "https://www.geeksforgeeks.org/how-to-convert-datetime-to-integer-in-python/"
 
 link_audio_voorbeelden = "/Users/yanickidsinga/Documents/GitHub/code_scriptie/data/Voorbeeld_data_audio.csv"
@@ -10,5 +12,13 @@ print(df)
 for i in df.index:
     print(df.loc[i, "Tijd begin detectie"],'\n')"""
 
-df = time_to_seconds(link_audio_voorbeelden)
+df = read_data(link_video_voorbeelden)
 print(df)
+hours_to_seconds(df, "Tijd foto")
+print(df)
+seconds_to_hours(df, "Tijd foto")
+print(df)
+dates_to_seconds(df, "Datum")
+print(df)
+print(select_row_species(df, "Soort", "Rattus Norvegicus"))
+print(select_ID(df, "ID", 6))
